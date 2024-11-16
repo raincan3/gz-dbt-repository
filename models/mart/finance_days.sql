497cb5f91967d7b473cc8262b70e78fc7ed4a22d
Rainier Cantre <rainier.cantre@gmail.com>
2024-11-16 09:34:12 +0000

    updating models, correcting bugs

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -5,8 +5,8 @@ select
     round(avg(revenue), 2) as average_basket,
     round(sum(operational_margin), 2) as operational_margin,
     round(sum(purchase_cost), 2) as total_purchase_cost,
-    round(sum(shipping_fee), 2) as total_shipping_fees,
-    round(sum(logcost), 2) as total_log_costs,
+    round(sum(s.shipping_fee), 2) as total_shipping_fees,
+    round(sum(s.logcost), 2) as total_log_costs,
     cast(sum(quantity) as int64) as total_quantity_sold
 from {{ref("int_orders_operational")}} o
 join {{ref("int_orders_margin")}} m
