c2e53fda636226900f7f0ea42de543fec361cad8
Rainier Cantre <rainier.cantre@gmail.com>
2024-11-16 09:01:24 +0000

    add finance days model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -4,7 +4,10 @@ select
     round(
         SUM(margin) + SUM(shipping_fee) - SUM(logcost) - SUM(ship_cost), 
         2
-    ) as operational_margin
+    ) as operational_margin,
+    round(sum(shipping_fee), 2) as shipping_fee,
+    round(sum(logcost), 2) as logcost,
+    round(sum(ship_cost), 2) as ship_cost
 from {{ref("int_orders_margin")}} m
 join {{ref("stg_raw__ship")}} s
 using (orders_id)
---
+++ models/mart/finance_days.sql
@@ -0,0 +1,17 @@
+select
+    o.date_date,
+    count(distinct orders_id) as nb_transactions,
+    round(sum(revenue), 2) as revenue,
+    round(avg(revenue), 2) as average_basket,
+    round(sum(operational_margin), 2) as operational_margin,
+    round(sum(purchase_cost), 2) as total_purchase_cost,
+    round(sum(shipping_fee), 2) as total_shipping_fees,
+    round(sum(logcost), 2) as total_log_costs,
+    cast(sum(quantity) as int64) as total_quantity_sold
+from {{ref("int_orders_operational")}} o
+join {{ref("int_orders_margin")}} m
+using (orders_id)
+join {{ref("stg_raw__ship")}} s
+using (orders_id)
+group by o.date_date
+order by o.date_date
